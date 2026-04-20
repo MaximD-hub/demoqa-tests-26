78fb06f9eb7470ef14522ee89abfa3ac5fe5ba44
Max Dedyura <Maxim.Dedyura@kaspi.kz>
2026-04-20 19:10:45 +0500

    added new test "AutomationPracticeForm"

--- src/test/java/tests/AutomationPracticeForm.java
+++ src/test/java/tests/AutomationPracticeForm.java
@@ -1,5 +1,55 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByTagAndText;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class AutomationPracticeForm {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void automationPracticeForm() {
+        open("/automation-practice-form");
+        $("#firstName").setValue(TestData.FIRST_NAME);
+        $("#lastName").setValue(TestData.SECOND_NAME);
+        $("#userEmail").setValue(TestData.EMAIL);
+
+        $(byTagAndText("label","Male")).click();
+
+        $("#userNumber").setValue(TestData.MOBILE_NUMBER);
+
+        // Выбор подходящей даты (с учетом что в сетке могут быть 2 одинаковые даты, но ра разные месяца)
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").click();
+        $(byTagAndText("option","2026")).click();
+        $(".react-datepicker__month-select").click();
+        $(byTagAndText("option","October")).click();
+        $x("//div[contains(@aria-label, 'October') and text()='30']").click();
+
+        $("#subjectsInput").setValue("Math, Physics, History");
+
+        $("#hobbies-checkbox-1").click();
+        $("#hobbies-checkbox-3").click();
+
+        //Загрузка фотографии
+        $("#uploadPicture").uploadFile(TestData.PICTURE_FILE);
+
+        $("#currentAddress").setValue(TestData.CURRENT_ADDRESS);
+
+        $("#state").click();
+
+
+
+    }
 }
--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.io.File;
+
 public class TestData {
     public static final String FIRST_NAME = "MAX";
     public static final String SECOND_NAME = "DEDYURA";
@@ -7,6 +9,10 @@ public class TestData {
 
     public static final String EMAIL = "m.dedyura@icloud.com";
 
+    public static final String MOBILE_NUMBER = "1234567890";
+
     public static final String CURRENT_ADDRESS = "253 Tole Bi St., Almaty, Kazakhstan";
     public static final String PERMANENT_ADDRESS = CURRENT_ADDRESS;
+
+    public static final File PICTURE_FILE = new File("src/test/resources/picture.jpeg");
 }
