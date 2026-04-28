084b974e85d0c2b9d6bceaa358e41a1a19084657
Max Dedyura <Maxim.Dedyura@kaspi.kz>
2026-04-28 19:17:34 +0500

    optimize RegistrationPage.java optimize PracticeFormWithPageObject.java

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -2,7 +2,10 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 import pages.components.CalendarComponent;
+import java.io.File;
 
+
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -15,40 +18,113 @@ public class RegistrationPage  {
             emailInput = $("#email"),
             genderWrapper = $("#genterWrapper"),
             userNumberInput = $("#userNumber"),
-            calendarInput = $("#dateOfBirthInput");
+            calendarInput = $("#dateOfBirthInput"),
+            subjectInput = $("#subjectsInput"),
+            subjectsValues = $(".subjects-auto-complete__multi-value"),
+            sportsCheckbox = $("#hobbies-checkbox-1"),
+            musicCheckbox = $("#hobbies-checkbox-3"),
+            uploadPicture = $("#uploadPicture"),
+            currentAddress = $("#currentAddress"),
+            stateCity = $("#state"),
+            haryanaOption = $("#react-select-3-option-3"),
+            city = $("#city"),
+            karnalOption = $("#react-select-4-option-1"),
+            submit = $("#submit"),
+            resultModal = $(".modal-content"),
+            resultTitle = $("#example-modal-sizes-title-lg");
+
 
     CalendarComponent calendarComponent = new CalendarComponent();
 
-    public void openPage() {
+    public RegistrationPage openPage() {
         open("/automation-practice-form");
         $(".text-center").shouldHave(text("Practice Form"));
+
+        return this;
     }
+
     public RegistrationPage setFirstName(String value) {
         firstNameInput.setValue(value);
 
         return this;
     }
+
     public RegistrationPage setLastName(String value) {
         lastNameInput.setValue(value);
 
-        return new RegistrationPage();
+        return this;
     }
 
-    public void setEmail(String value) {
+    public RegistrationPage setEmail(String value) {
         emailInput.setValue(value);
+
+        return this;
     }
 
-    public void setGender(String value) {
+    public RegistrationPage setGender(String value) {
         genderWrapper.$(byText(value)).click();
+
+        return this;
     }
 
-    public void setUserNumber(String value) {
+    public RegistrationPage setUserNumber(String value) {
         userNumberInput.setValue(value);
+
+        return this;
     }
 
     public RegistrationPage setDateOfBirth(String day, String month, String year) {
         calendarInput.click();
         calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationPage setSubject (String value1, String value2) {
+        subjectInput.setValue(value1).pressTab();
+        subjectsValues.shouldHave(text(value2));
+
+        return this;
+    }
+
+    public RegistrationPage setHobbies () {
+        sportsCheckbox.click();
+        musicCheckbox.click();
+
+        return this;
+    }
+
+    public RegistrationPage setPicture(File file) {
+        uploadPicture.uploadFile(file);
+
+        return this;
+    }
+
+    public RegistrationPage setCurrentAddress(String value) {
+        currentAddress.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setStateCity() {
+
+        stateCity.scrollIntoView(true).click();
+        haryanaOption.click();
+        city.click();
+        karnalOption.click();
+
+        return this;
+    }
+
+    public RegistrationPage clickSubmit () {
+        submit.click();
+
+        return this;
+    }
+
+    public RegistrationPage verifySuccessModal (String message) {
+        resultModal.shouldBe(appear);
+        resultTitle.shouldHave(text(message));
 
         return this;
     }
--- src/test/java/tests/PracticeFormWithPageObject.java
+++ src/test/java/tests/PracticeFormWithPageObject.java
@@ -22,40 +22,18 @@ public class PracticeFormWithPageObject extends TestBase {
 
         registrationPage.setFirstName(TestData.FIRST_NAME)
                 .setLastName(TestData.SECOND_NAME)
-                .setEmail(TestData.EMAIL);
-
-        registrationPage.setGender("Male");
-
-        registrationPage.setUserNumber(TestData.MOBILE_NUMBER);
-
-        registrationPage.setDateOfBirth("30","October","2026");
-
-
-
-
-        $("#subjectsInput").setValue("M").pressTab();
-        $(".subjects-auto-complete__multi-value").shouldHave(text("Maths"));
-                // " Physics, History");
-
-        $("#hobbies-checkbox-1").click();
-        $("#hobbies-checkbox-3").click();
-
-        //Загрузка фотографии
-        $("#uploadPicture").uploadFile(TestData.PICTURE_FILE);
-
-        $("#currentAddress").setValue(TestData.CURRENT_ADDRESS);
-
-        $("#state").scrollIntoView(true).click();
-        $("#react-select-3-option-3").click();
-        $("#city").click();
-        $("#react-select-4-option-1").click();
-
-        $("#submit").pressEnter();
-
-        $(".modal-content").shouldBe(appear);
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-
-        registrationPage.checkResult("Student Name",TestData.FULL_NAME)
+                .setEmail(TestData.EMAIL)
+                .setGender("Male")
+                .setUserNumber(TestData.MOBILE_NUMBER)
+                .setDateOfBirth("30","October","2026")
+                .setSubject("M", "Maths")
+                .setHobbies()
+                .setPicture(TestData.PICTURE_FILE)
+                .setCurrentAddress(TestData.CURRENT_ADDRESS)
+                .setStateCity()
+                .clickSubmit()
+                .verifySuccessModal("Thanks for submitting the form")
+                .checkResult("Student Name",TestData.FULL_NAME)
                 .checkResult("Student Email",TestData.EMAIL);
 
 
