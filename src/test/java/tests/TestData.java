d07405c23ce65f9bae90d62ff77edf0a746eb5c2
Max Dedyura <Maxim.Dedyura@kaspi.kz>
2026-04-30 14:48:43 +0500

    optimize

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -3,16 +3,13 @@ package tests;
 import java.io.File;
 
 public class TestData {
-    public static final String FIRST_NAME = "MAX";
-    public static final String SECOND_NAME = "DEDYURA";
-    public static final String FULL_NAME = FIRST_NAME + " " + SECOND_NAME;
-
-    public static final String EMAIL = "m.dedyura@icloud.com";
-
-    public static final String MOBILE_NUMBER = "1234567890";
-
-    public static final String CURRENT_ADDRESS = "253 Tole Bi St., Almaty, Kazakhstan";
-    public static final String PERMANENT_ADDRESS = CURRENT_ADDRESS;
+    public static final String FIRST_NAME = "MAX",
+            SECOND_NAME = "DEDYURA",
+            FULL_NAME = FIRST_NAME + " " + SECOND_NAME,
+            EMAIL = "m.dedyura@icloud.com",
+            MOBILE_NUMBER = "1234567890",
+            CURRENT_ADDRESS = "253 Tole Bi St., Almaty, Kazakhstan",
+            PERMANENT_ADDRESS = CURRENT_ADDRESS;
 
     public static final File PICTURE_FILE = new File("src/test/resources/picture.jpeg");
 }
